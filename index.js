d9ba3393b8d35834cb127d09d8d862ebf4823e64
Conway Hogan <conway2019hogan@gmail.com>
2023-11-25 11:21:29 -0700

    starting to work on login

--- index.js
+++ index.js
@@ -21,8 +21,15 @@ var cheapCooks = 'AppContents/';
 
 var stylesheets = '';
 
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.set('views', '/Users/conwayhogan/Desktop/403/403RecipesApp/AppContents/views')
+
 app.use(express.static(path.join(stylesheets, 'AppContents')));
 
 app.get("/", (req, res) => res.sendFile(path.join(cheapCooks, 'index.html')));
+
+app.get('/register', (req, res) => { res.render('register.ejs')});
 
 app.listen(3000, () => console.log("The server is listening for a client."));
