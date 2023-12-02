d61abb4d9ad2707ca338944ad44580145df63529
Conway Hogan <conway2019hogan@gmail.com>
2023-12-01 19:45:31 -0700

    changing the certificate to is404 site

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d RecipesCheap.us-east-1.elasticbeanstalk.com  --nginx --agree-tos --email cjh242@byu.edu
+sudo certbot -n -d recipes.is404.net  --nginx --agree-tos --email cjh242@byu.edu
